e7ea45b03d65bed259e411938d81fc5b377dcda8
joojiwon <85204528+joojiwon@users.noreply.github.com>
2021-11-26 23:28:32 +0900

    css 1

--- src/weather.js
+++ src/weather.js
@@ -5,8 +5,8 @@ function ongeoOk(position){
     const lng = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`;
     fetch(url).then(Response => Response.json()).then(data => {
-        const weather = document.querySelector("#weather span:first-child");
-        const city = document.querySelector("#weather span:last-child");
+        const weather = document.querySelector("#weather div:first-child");
+        const city = document.querySelector("#weather div:last-child");
        city.innerText = data.name;
       weather.innerText = data.weather[0].main;
     });
